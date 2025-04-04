12956994d47e00fa7bdc54b514b4ce4270ab50c3
Vitor-Calliari <vitorcalliari@hotmail.com>
2025-04-04 20:24:41 -0300

    Crud de usuário

--- src/main/java/br/com/bpkedu/library_spring_webmvc/domain/Usuario.java
+++ src/main/java/br/com/bpkedu/library_spring_webmvc/domain/Usuario.java
@@ -12,4 +12,70 @@ public class Usuario {
     private Long id;
     private String nome;
     private String senha;
+    private String email;
+    private String telefone;
+    private String endereco;
+
+    public Usuario() {
+    }
+
+    public Usuario(String nome, String senha, String email, String telefone, String endereco) {
+    }
+
+    public Usuario(Long id, String nome, String senha, String email, String telefone, String endereco) {
+        this.id = id;
+        this.nome = nome;
+        this.senha = senha;
+        this.email = email;
+        this.telefone = telefone;
+        this.endereco = endereco;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
 }
--- src/main/java/br/com/bpkedu/library_spring_webmvc/service/UsuarioService.java
+++ src/main/java/br/com/bpkedu/library_spring_webmvc/service/UsuarioService.java
@@ -1,6 +1,7 @@
 package br.com.bpkedu.library_spring_webmvc.service;
 
 import br.com.bpkedu.library_spring_webmvc.domain.Usuario;
+import br.com.bpkedu.library_spring_webmvc.dto.UsuarioDTO;
 import br.com.bpkedu.library_spring_webmvc.repository.UsuarioRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -15,7 +16,12 @@ public class UsuarioService {
 
 
     public List<Usuario> listarTodos(){
-       return usuarioRepository.findAll();
+        return usuarioRepository.findAll();
     }
 
+    public Usuario buscarPorId(Long id){return usuarioRepository.findById(id).orElse(null);}
+
+    public Usuario salvar(Usuario usuario){return usuarioRepository.save(usuario);}
+
+    public void deletar(Long id){usuarioRepository.deleteById(id);}
 }
